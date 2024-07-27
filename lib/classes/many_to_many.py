38d12bf63974488605f92b82498c77b016b199fa
Tonny Mulli <tonnymulli2118@gmail.com>
2024-07-28 00:36:30 +0300

    finish lab

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,40 +1,32 @@
-class Author:
-    def __init__(self, name):
-        assert isinstance(name, str) and len(name) > 0
-        self._name = name
-        self._articles = []
+class Article:
+    all = []
+
+    def __init__(self, author, magazine, title):
+        if not isinstance(title, str):
+            raise ValueError("Title must be a string.")
+        if not (5 <= len(title) <= 50):
+            raise ValueError("Title length must be between 5 and 50 characters.")
+        self._title = title
+        self.author = author
+        self.magazine = magazine
+        Article.all.append(self)
 
     @property
-    def name(self):
-        return self._name
+    def title(self):
+        return self._title
 
-    def articles(self):
-        return self._articles
-
-    def magazines(self):
-        return list(set(article.magazine for article in self._articles))
-
-    def add_article(self, magazine, title):
-        article = Article(self, magazine, title)
-        self._articles.append(article)
-        return article
-
-    def topic_areas(self):
-        if not self._articles:
-            return None
-        return list(set(magazine.category for magazine in self.magazines()))
+    @title.setter
+    def title(self, value):
+        raise AttributeError("Title is immutable.")
 
 
-class Magazine:
-    _instances = []
-
-    def __init__(self, name, category):
-        assert isinstance(name, str) and 2 <= len(name) <= 16
-        assert isinstance(category, str) and len(category) > 0
+class Author:
+    def __init__(self, name):
+        if not isinstance(name, str):
+            raise ValueError("Name must be a string.")
+        if not name:
+            raise ValueError("Name cannot be empty.")
         self._name = name
-        self._category = category
-        self._articles = []
-        self.__class__._instances.append(self)
 
     @property
     def name(self):
@@ -42,7 +34,49 @@ class Magazine:
 
     @name.setter
     def name(self, value):
-        assert isinstance(value, str) and 2 <= len(value) <= 16
+        raise AttributeError("Name is immutable.")
+
+    def articles(self):
+        return [article for article in Article.all if article.author == self]
+
+    def magazines(self):
+        return list({article.magazine for article in self.articles()})
+
+    def add_article(self, magazine, title):
+        return Article(self, magazine, title)
+
+    def topic_areas(self):
+        if not self.articles():
+            return None
+        return list({magazine.category for magazine in self.magazines()})
+
+
+class Magazine:
+    all = []
+
+    def __init__(self, name, category):
+        if not isinstance(name, str):
+            raise ValueError("Name must be a string.")
+        if not (2 <= len(name) <= 16):
+            raise ValueError("Name length must be between 2 and 16 characters.")
+        if not isinstance(category, str):
+            raise ValueError("Category must be a string.")
+        if not category:
+            raise ValueError("Category must not be empty.")
+        self._name = name
+        self._category = category
+        Magazine.all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise ValueError("Name must be a string.")
+        if not (2 <= len(value) <= 16):
+            raise ValueError("Name length must be between 2 and 16 characters.")
         self._name = value
 
     @property
@@ -51,62 +85,29 @@ class Magazine:
 
     @category.setter
     def category(self, value):
-        assert isinstance(value, str) and len(value) > 0
+        if not isinstance(value, str):
+            raise ValueError("Category must be a string.")
+        if not value:
+            raise ValueError("Category must not be empty.")
         self._category = value
 
     def articles(self):
-        return self._articles
+        return [article for article in Article.all if article.magazine == self]
 
     def contributors(self):
-        return list(set(article.author for article in self._articles))
+        return list({article.author for article in self.articles()})
+
+    def article_titles(self):
+        titles = [article.title for article in self.articles()]
+        return titles if titles else None
+
+    def contributing_authors(self):
+        authors = [article.author for article in self.articles()]
+        contributing_authors = [author for author in set(authors) if authors.count(author) > 2]
+        return contributing_authors if contributing_authors else None
 
     @classmethod
     def top_publisher(cls):
-        if not cls._instances:
+        if not Article.all:
             return None
-        return max(cls._instances, key=lambda magazine: len(magazine.articles()))
-
-    def article_titles(self):
-        if not self._articles:
-            return None
-        return [article.title for article in self._articles]
-
-    def contributing_authors(self):
-        authors = [article.author for article in self._articles]
-        return [author for author in set(authors) if authors.count(author) > 2]
-
-
-class Article:
-    def __init__(self, author, magazine, title):
-        assert isinstance(title, str) and 5 <= len(title) <= 50
-        assert isinstance(author, Author)
-        assert isinstance(magazine, Magazine)
-        self._title = title
-        self._author = author
-        self._magazine = magazine
-        magazine._articles.append(self)
-        author._articles.append(self)
-
-    @property
-    def title(self):
-        return self._title
-
-    @property
-    def author(self):
-        return self._author
-
-    @author.setter
-    def author(self, value):
-        assert isinstance(value, Author)
-        self._author = value
-        value._articles.append(self)
-
-    @property
-    def magazine(self):
-        return self._magazine
-
-    @magazine.setter
-    def magazine(self, value):
-        assert isinstance(value, Magazine)
-        self._magazine = value
-        value._articles.append(self)
+        return max(cls.all, key=lambda magazine: len(magazine.articles()))
